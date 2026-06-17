677121bab3698502aaadfadb11e9caa528e1b588
alhbib699 <alhbib699@gmail.com>
2026-06-17 18:23:37 +0300

    feat: add filtering and sorting for cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,13 +3,29 @@ import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { cabins, isLoading } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  //1) FILTER
+  const filterValue = searchParams.get("discount") || "all";
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  //2)SORTING
+
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1
+  const sortedCabins = filteredCabins.sort((a,b) => (a[field] - b[field]) * modifier);
 
   return (
     <Menus>
@@ -23,7 +39,9 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          // data={cabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,36 +1,26 @@
-import SortBy from 'ui/SortBy';
-import Filter from 'ui/Filter';
-import TableOperations from 'ui/TableOperations';
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
     <TableOperations>
-      {/* We could do these two as compound components as well, but let's keep it simple, and let's also explore different ways of achieving the same thing */}
       <Filter
-        filterField='discount'
+        filterField="discount"
         options={[
-          { value: 'all', label: 'All' },
-          { value: 'no-discount', label: 'No discount' },
-          { value: 'with-discount', label: 'With discount' },
+          { value: "all", label: "All" },
+          { value: "no-discount", label: "No discount" },
+          { value: "with-discount", label: "With discount" },
         ]}
       />
-
-      <SortBy
-        options={[
-          { value: 'name-asc', label: 'Sort by name (A-Z)' },
-          { value: 'name-desc', label: 'Sort by name (Z-A)' },
-          { value: 'regularPrice-asc', label: 'Sort by price (low first)' },
-          {
-            value: 'regularPrice-desc',
-            label: 'Sort by price (high first)',
-          },
-          { value: 'maxCapacity-asc', label: 'Sort by capacity (low first)' },
-          {
-            value: 'maxCapacity-desc',
-            label: 'Sort by capacity (high first)',
-          },
-        ]}
-      />
+      <SortBy options={[
+        {value:'name-asc',label:'Sorted by name (A-Z)'},
+        {value:'name-desc',label:'Sorted by name (Z-A)'},
+        {value:'regularPrice-asc',label:'Sorted by price (low first)'},
+        {value:'regularPrice-desc',label:'Sorted by price (high first)'},
+        {value:'maxCapacity-asc',label:'Sorted by capacity (low first)'},
+        {value:'maxCapacity-desc',label:'Sorted by capacity (high first)'},
+      ]}/>
     </TableOperations>
   );
 }
