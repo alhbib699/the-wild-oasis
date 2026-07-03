1ddbd0517c164b42ef9578a7ae7fe693531cd7a3
alhbib699 <alhbib699@gmail.com>
2026-07-03 20:12:55 +0300

    feat: implement user account update functionality

--- src/features/authentication/useUser.js
+++ src/features/authentication/useUser.js
@@ -1,12 +1,12 @@
-import { useQuery } from "@tanstack/react-query"
-import { getCurrentUser } from "../../services/apiAuth"
+import { useQuery } from "@tanstack/react-query";
+import { getCurrentUser } from "../../services/apiAuth";
 
 function useUser() {
-    const {data: user, isLoading} = useQuery({
-        queryKey:["user"],
-        queryFn:getCurrentUser
-    })
-    return {isLoading , user ,isAuthenticated: user?.role === "authenticated"}
+  const { data: user, isLoading } = useQuery({
+    queryKey: ["user"],
+    queryFn: getCurrentUser,
+  });
+  return { isLoading, user, isAuthenticated: user?.role === "authenticated" };
 }
 
-export default useUser
+export default useUser;
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,20 +1,23 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
-export async function signup({fullName, email, password}) {
+export async function signup({ fullName, email, password }) {
   //this options methods is used to pass additional configuration and user metadata during registration.
-  const {data, error} = await supabase.auth.signUp({email,password , options:{
-    data:{
-      fullName,
-      avatar:""
-    }
-  }})
-   if (error) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+  if (error) {
     throw new Error(error.message);
   }
 
   return data;
 }
-
 
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
@@ -45,6 +48,33 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
 
   if (error) throw new Error(error.message);
+}
 
+export async function updateCurrentUser({ fullName, password, avatar }) {
+  //1. Update password OR fullname
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
 
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  //2. upload avatar in image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+  if (storageError) throw new Error(storageError.message);
+
+  //update avatar in the user
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+  if (error2) throw new Error(error2.message);
+
+  return updatedUser;
 }
